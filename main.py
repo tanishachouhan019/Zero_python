988a0007ef4f490f0eedf9c9a733868370c38ce5
Tanisha Chouhan <tanishachouhan019@gmail.com>
2026-05-09 00:05:14 +0530

    mini projects

---
+++ main.py
@@ -0,0 +1,112 @@
+#project - CRUD Operation 
+import os
+from pathlib import Path
+def readfileandfolder():
+    try:
+        p = Path('')
+        items = list(p.rglob('*'))
+        for index , file in enumerate(items):
+            print(f'{index+1} - {file}')
+    except Exception as e:
+        print(e)
+
+
+def create_file():
+    try:
+        readfileandfolder()
+        #D:\FILE HANDING\main.py
+        file_name = input('entre name of the file: ')
+        p = Path(file_name)
+        if p.exists():
+            print('FILE ALREADY EXISTS')
+        else:
+            with open(file_name , 'w') as file:
+                content = input('entre your file content: ')
+                file.write(content)
+                print('FILE ADDED!')
+    except Exception as e:
+        print(e)
+
+def read_file():
+    try:
+        readfileandfolder()
+        file_name = input('Entre name of your file: ')
+        p = Path(file_name)
+        if p.exists():
+            with open(file_name , 'r') as file:
+                print(file.read())
+        else:
+            print('FILE NOT FOUND')
+    except Exception as e:
+        print(e)
+
+
+while True:
+    print("Press 1 for creating a file")
+    print("Press 2 for reading a file")
+    print("Press 3 for updating a file")
+    print("Press 4 for deleting a file")
+    print("Press 0 for existing...")
+
+    option = int(input("Entre your choice"))
+    if option == 1:
+        create_file()
+
+    if option ==2:
+        read_file()
+
+    if option == 3:
+        update_file()
+
+    if option == 4:
+        delete_file()
+
+    if option == 0:
+        break
+
+#--------
+#Exception handing
+#2 block hote h
+#try
+#except
+
+def update_file():
+    try:
+        readfileandfolder()
+        file_name = input('Entre name of your file: ')
+        p = Path(file_name)
+        
+        if p.exists():
+            print('Press 1 to overwrite the content')
+            print('Press 2 to append new content')
+
+            option = int(input('entre your choice for updating file: '))
+            if option == 1:
+                with open(file_name, 'w') as file:
+                    content = input('Entre your content')
+                    file.write(content)
+                    print('CONTENT CHANGED....')
+
+            elif option == 2:
+                with open(file_name, 'a') as file:
+                    content = input('Entre your content')
+                    file.write(content)
+                    print('CONTENT CHANGED....')
+
+            else:
+                print("INVALID INPUT")
+        else:
+            print("FILE DOESN'T EXISTS!")
+    
+    except Exception as e:
+        print(e)
+
+def delete_file():
+    readfileandfolder()
+    file_name = input('Entre name of your file: ')
+    p = Path(file_name)
+    if p.exists():
+        os.remove(p) #oS is removing path of that file completely from the system
+        print("FILE DELETED")
+    else:
+        print('FILE DOES NOT EXISTS!!')
