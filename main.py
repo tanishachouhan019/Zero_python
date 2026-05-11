99b6f238d5039dbc1f6a147b823ca82858954b04
Tanisha Chouhan <tanishachouhan019@gmail.com>
2026-05-12 00:17:01 +0530

    main.py
    CRUD Operation

--- main.py
+++ main.py
@@ -39,37 +39,7 @@ def read_file():
             print('FILE NOT FOUND')
     except Exception as e:
         print(e)
-
-
-while True:
-    print("Press 1 for creating a file")
-    print("Press 2 for reading a file")
-    print("Press 3 for updating a file")
-    print("Press 4 for deleting a file")
-    print("Press 0 for existing...")
-
-    option = int(input("Entre your choice"))
-    if option == 1:
-        create_file()
-
-    if option ==2:
-        read_file()
-
-    if option == 3:
-        update_file()
-
-    if option == 4:
-        delete_file()
-
-    if option == 0:
-        break
-
-#--------
-#Exception handing
-#2 block hote h
-#try
-#except
-
+#--
 def update_file():
     try:
         readfileandfolder()
@@ -101,6 +71,7 @@ def update_file():
     except Exception as e:
         print(e)
 
+
 def delete_file():
     readfileandfolder()
     file_name = input('Entre name of your file: ')
@@ -110,3 +81,70 @@ def delete_file():
         print("FILE DELETED")
     else:
         print('FILE DOES NOT EXISTS!!')
+
+def rename_file():
+    readfileandfolder()
+    file_name = input('Entre name of your file:')
+    p = Path(file_name)
+    if p.exists():
+        new_file = input('Entre new name of your file: ')
+        p.rename(new_file)
+        print('FILE RENAMED!')
+    else:
+        print('FILE NOT FOUND')
+
+def create_folder():
+    readfileandfolder()
+    folder_name = input("Entre name of your folderr: ")
+    p = Path(folder_name)
+    if p.exists():
+        print('FOLDER ALREADY EXISTS!')
+    else:
+        p.mkdir()
+        print('FOLDER CREATED !')
+
+def delete_folder():
+    readfileandfolder()
+    folder_name = input("Entre name of your folderr: ")
+    p = Path(folder_name)
+    if p.exists():
+        p.rmdir()
+        print('FOLDER DELETED!')
+    else:
+        print('FOLDER NOT FOUND !')
+
+
+while True:
+    print("Press 1 for creating a file")
+    print("Press 2 for reading a file")
+    print("Press 3 for updating a file")
+    print("Press 4 for deleting a file")
+    print("Press 5 for renaming a file")
+    print("Press 6 for creating a folder")
+    print("Press 7 for delete a folder")
+    print("Press 0 for existing...")
+
+    option = int(input("Entre your choice"))
+    if option == 1:
+        create_file()
+
+    if option ==2:
+        read_file()
+
+    if option == 3:
+        update_file()
+
+    if option == 4:
+        delete_file()
+
+    if option == 5:
+        rename_file()
+  
+    if option == 6:
+        create_folder()
+
+    if option == 7:
+        delete_folder()
+
+    if option == 0:
+        break
